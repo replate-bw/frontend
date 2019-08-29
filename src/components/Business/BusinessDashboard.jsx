a9b97dce55b11b46366e408b4a7354a28c9842b8
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-08-29 14:26:00 -0700

    minor style changes on BusinessDashboard

--- src/components/Business/BusinessDashboard.jsx
+++ src/components/Business/BusinessDashboard.jsx
@@ -10,7 +10,9 @@ import { axiosWithAuth } from "../../utils/axiosWithAuth";
 const BusinessDashboard = props => {
   const id = props.match.params.id;
 
-  const { user, setUser, setAppToEdit, locations, setLocations } = useContext(UserContext);
+  const { user, setUser, setAppToEdit, locations, setLocations } = useContext(
+    UserContext
+  );
 
   const [appointments, setAppointments] = useState([]);
 
@@ -24,7 +26,6 @@ const BusinessDashboard = props => {
       .catch(err => console.log(err));
   }, []);
 
-
   useEffect(() => {
     axiosWithAuth()
       .get("https://replatedb.herokuapp.com/appointments/mine")
@@ -36,32 +37,30 @@ const BusinessDashboard = props => {
 
   useEffect(() => {
     axiosWithAuth()
-    .get("https://replatedb.herokuapp.com/locations/")
-    .then(res => {
-      console.log(res)
-    })
-    .catch(err => console.log(err));
-  }, [])
+      .get("https://replatedb.herokuapp.com/locations/")
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.log(err));
+  }, []);
 
   const deleteLocation = item => {
     axiosWithAuth()
-    .delete(`https://replatedb.herokuapp.com/locations/${item.id}`)
-    .then(res => {
-      console.log(res)
-      setLocations(locations.filter(loc => loc.id !== item.id))
-    })
-    .catch(err => console.log(err));
-  }
+      .delete(`https://replatedb.herokuapp.com/locations/${item.id}`)
+      .then(res => {
+        console.log(res);
+        setLocations(locations.filter(loc => loc.id !== item.id));
+      })
+      .catch(err => console.log(err));
+  };
   const deleteAppointment = item => {
     axiosWithAuth()
-    .delete(`https://replatedb.herokuapp.com/appointments/${item.id}`)
-    .then(res => {
-      console.log(res)
-    })
-    .catch(err => console.log(err));
-  }
-
-
+      .delete(`https://replatedb.herokuapp.com/appointments/${item.id}`)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.log(err));
+  };
 
   const buttonHover = e => {
     const btn = e.target;
@@ -92,16 +91,21 @@ const BusinessDashboard = props => {
                       <br />
                       {loc.city}, {loc.state} {loc.zip}
                     </p>
-                    <button onClick={() => deleteLocation(loc)} className='dashboard-function'>X</button>
+                    <button
+                      onClick={() => deleteLocation(loc)}
+                      className="dashboard-function"
+                    >
+                      X
+                    </button>
                   </div>
                 </div>
               ))}
 
               <Link
-			  	to={`/protected/business/new-location/${id}`}
+                to={`/protected/business/new-location/${id}`}
                 onMouseEnter={buttonHover}
                 onMouseLeave={buttonReturn}
-				className="dashboard-button add-location__button"
+                className="dashboard-button add-location__button"
               >
                 <FontAwesomeIcon icon={faPlusCircle} />
                 New
@@ -121,21 +125,34 @@ const BusinessDashboard = props => {
               <FontAwesomeIcon icon={faPlus} />
               Make A Donation
             </Link>
-			{appointments.map(app => (
-				 <div className="dashboard-location">
-				 <div className="location-text">
-				   <p className="location-info">
-					 {app.time}
-					 <br />
-					 {app.quantity}
-					 <br />
-					 {app.type}
-				   </p>
-				 </div>
-         <button onClick={() => deleteAppointment(app)} className='dashboard-delete'>X</button>
-         <Link className='dashboard-function' onClick={() => setAppToEdit(app)} to={`/protected/business/edit-pickup/${id}`}>Edit</Link>
-			   </div>
-			))}
+            <div className="dashboard-locations">
+              {appointments.map(app => (
+                <div className="dashboard-location">
+                  <div className="location-text">
+                    <p className="location-info">
+                      {app.time}
+                      <br />
+                      {app.quantity}
+                      <br />
+                      {app.type}
+                    </p>
+                  </div>
+                  <button
+                    onClick={() => deleteAppointment(app)}
+                    className="dashboard-delete"
+                  >
+                    X
+                  </button>
+                  <Link
+                    className="dashboard-function"
+                    onClick={() => setAppToEdit(app)}
+                    to={`/protected/business/edit-pickup/${id}`}
+                  >
+                    Edit
+                  </Link>
+                </div>
+              ))}
+            </div>
           </div>
           <div className="dashboard-section">
             <h3 className="dashboard-subheader">Next Week's Schedule</h3>
